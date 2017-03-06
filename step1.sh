aea5f26a0c02af8b47ec0e1e7e2b1724802add9b
Alex <you@example.com>
2017-03-06 12:50:24 +0000

    Added comments

--- step1.sh
+++ step1.sh
@@ -1,7 +1,7 @@
 # First script to run in order to set up the enviroment for fiware
 # Disabling selinux and installing packages for fiware
 
-
+echo "Installing packages..."
 yum install $(cat rpm-qa-dump)
 
 echo "SELINUX=disabled   SELINUXTYPE=targeted" >  /etc/sysconfig/selinux
