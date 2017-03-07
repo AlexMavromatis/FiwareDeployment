4b4d16fa618d5cc609fd90666f2091fa2a2b7041
Alex <you@example.com>
2017-03-07 12:31:01 +0000

    Fully configured library for endpoints #1

--- library.sh
+++ library.sh
@@ -1,3 +1,22 @@
+#!/bin/sh
+# /*************************************************************************
+#
+#  University of Bristol Confidential
+#  __________________
+#
+#
+#  All Rights Reserved.
+#
+#  NOTICE:  All information contained herein is, and remains
+#  the property of University of Bristol and its suppliers,
+#  if any.  The intellectual and technical concepts contained
+#  herein are proprietary to University of Bristol
+#  and its suppliers and may be covered by U.K. and Foreign Patents,
+#  patents in process, and are protected by trade secret or copyright law.
+#  Dissemination of this information or reproduction of this material
+#  is strictly forbidden unless prior written permission is obtained
+#  from University of Bristol.
+
 epel="https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm"
 EXPORT epel
 
